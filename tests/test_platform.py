2b5b0cfed9fa6de744a0c2121aca029bc93dfc13
shaileshaanand <anaandshailu@gmail.com>
2020-07-14 13:59:26 +0530

    add test for exe

--- tests/test_platform.py
+++ tests/test_platform.py
@@ -1,4 +1,6 @@
 import unittest
+import pathlib
+import subprocess
 import sys
 import os
 
@@ -13,8 +15,8 @@ THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
 class TestPython(unittest.TestCase):
     def test_single(self):
-        python_file_path = os.path.join(
-            THIS_DIR, "test_programs", "test_python_sample.py")
+        python_file_path = pathlib.Path(os.path.join(
+            THIS_DIR, "test_programs", "test_python_sample.py")).resolve()
         process = platform.python(
             python_file_path, "Hello World".strip().encode())
         self.assertEqual(
@@ -23,12 +25,25 @@ class TestPython(unittest.TestCase):
 
 class TestJava(unittest.TestCase):
     def test_single(self):
-        java_file_path = os.path.join(
-            THIS_DIR, "test_programs", "test_java_sample.java")
+        java_file_path = pathlib.Path(os.path.join(
+            THIS_DIR, "test_programs", "test_java_sample.java")).resolve()
         process = platform.java(
             java_file_path, "Hello World".strip().encode())
         self.assertEqual(
             process.stdout.decode().strip().replace('\r', ''), "Hello World")
+
+
+class TestExe(unittest.TestCase):
+    def test_single(self):
+        exe_source_path = os.path.join(
+            THIS_DIR, "test_programs", "test_exe_sample.c")
+        exe_output_path = os.path.join(
+            THIS_DIR, "test_programs", "test_exe_sample.exe")
+        subprocess.run(["gcc", "-o", exe_output_path, exe_source_path])
+        process = platform.exe(
+            pathlib.Path(exe_output_path).resolve(), "HelloWorld".strip().encode())
+        self.assertEqual(
+            process.stdout.decode().strip().replace('\r', ''), "HelloWorld")
 
 
 if __name__ == "__main__":
---
+++ tests/test_programs/test_exe_sample.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    char str[255];
+    scanf("%s", str);
+    printf("%s", str);
+}
