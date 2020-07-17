3bda0fb1a4c44192aa7180ad4b0cf1c46d99562f
shaileshaanand <anaandshailu@gmail.com>
2020-07-17 15:01:24 +0530

    improve testing (#12)
    * use temporary fs in testing
    * add more tests for failed cases

--- tests/test_programs/test_java_sample.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-
-public class test_java_sample {
-	public static void main(String args[]) throws IOException {
-		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-		System.out.println(in.readLine());
-	}
-}
